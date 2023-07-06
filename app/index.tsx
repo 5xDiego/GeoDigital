dfbfceec7b6d4801467b43734e40ebb1ac54149c
5xDiego <schindelediego@gmail.com>
2023-07-06 20:20:45 +0200

    minor improvements

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import MapView from 'react-native-maps';
 import { useRouter } from "expo-router";
-import { StyleSheet, View, Button, Text } from "react-native";
-
+import { StyleSheet, View } from "react-native";
+import Button from './comps/Button';
+import CamButton from './comps/camButton';
 
 const WelcomeScreen = () => {
     const navigation = useRouter();
 
     return (
         <View style={styles.container}>
-            <Button title="camera" onPress={() => navigation.push('/qr_scanner')} />
-            <Button title="trophy" onPress={() => navigation.push('/trophy_room')} />
+            <MapView style={styles.map} >
+            </MapView>
+            <View style={styles.btnGroup}>
+                <Button label="Trophies" action={() => navigation.push("/trophy_room")} />
+                <Button label="Camera" action={() => navigation.push("/qr_scanner")} />
+            </View>
         </View>
     );
 };
@@ -30,6 +36,15 @@ const styles = StyleSheet.create({
         fontSize: 16,
         textAlign: "center",
     },
+    map: {
+        width: "100%",
+        height: "100%",
+    },
+    btnGroup: {
+        flex: 1,
+        position: "absolute",
+
+    }
 });
 
 export default WelcomeScreen;
--- app/qr_scanner/index.tsx
+++ app/qr_scanner/index.tsx
@@ -9,6 +9,7 @@ const qr_scanner = () => {
     const [scanned, setScanned] = useState(false);
     
     {/* source: https://snack.expo.dev/@eseg/simple-qr-code-scanner  */ }
+
     useEffect(() => {
         const getBarCodeScannerPermissions = async () => {
             const { status } = await BarCodeScanner.requestPermissionsAsync();
@@ -37,7 +38,8 @@ const qr_scanner = () => {
                 onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
                 style={StyleSheet.absoluteFillObject}
             />
-            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}     {/* source: https://snack.expo.dev/@eseg/simple-qr-code-scanner  */ }
+            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}    
+             {/* source: https://snack.expo.dev/@eseg/simple-qr-code-scanner  */ }
         </View>
     );
 }
